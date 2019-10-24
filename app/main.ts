774635354bac5e9c11d53755f6a598f7e859bd08
esride-nik <n.koehn@esri.de>
2019-10-24 13:38:21 +0200

    Complete functionality, class-based

--- app/main.ts
+++ app/main.ts
@@ -14,11 +14,15 @@ class LearnJsapi4App {
   private map : EsriMap;
   mapView: MapView;
   sceneView: SceneView;
+  weinLayer: FeatureLayer;
+  weinQuery: __esri.Query;
   
   constructor() {
+
     this.map = new EsriMap({
       basemap: "gray-vector"
     });
+    this.addWeinLayer();
 
     this.mapView = new MapView({
       map: this.map,
@@ -33,78 +37,79 @@ class LearnJsapi4App {
       zoom: 3
     });
 
+    this.addWidgets(this.mapView);
+    this.addWidgets(this.sceneView);
+    
+    this.mapView.when(() => {
+      this.weinLayer.queryExtent(this.weinQuery).then((result: any) => {
+        this.mapView.goTo(result.extent, {
+          animate: true,
+          duration: 10000,
+          easing: "ease-out"
+        });
+      });
+    });
+    
+    this.sceneView.when(() => {
+      this.weinLayer.queryExtent(this.weinQuery).then((result: any) => {
+        this.sceneView.goTo(result.extent, {
+          animate: true,
+          duration: 10000,
+          easing: "ease-out"
+        }).then(() => {
+          this.sceneView.watch("center", (c: Point) => {
+            this.mapView.center = c;
+          });
+        });
+      });
+    });
+    
+  }
+
+  private addWeinLayer() {
+    this.weinLayer = new FeatureLayer({
+      url: "http://services.arcgis.com/OLiydejKCZTGhvWg/arcgis/rest/services/WeinanbauGebiete/FeatureServer/0"
+    });
+    this.map.add(this.weinLayer);
+    this.weinQuery = this.weinLayer.createQuery();
+    this.weinQuery.where = "1=1";
+    this.weinQuery.outFields = ["*"];
   }
   
-  // this.weinLayer = new FeatureLayer({
-  //   url: "http://services.arcgis.com/OLiydejKCZTGhvWg/arcgis/rest/services/WeinanbauGebiete/FeatureServer/0"
-  // });
-  // map.add(weinLayer);
-  // let weinQuery = weinLayer.createQuery();
-  // weinQuery.where = "1=1";
-  // weinQuery.outFields = ["*"];
+  private addWidgets(view: View) {
+    var layerList = new LayerList({
+      view: view
+    });
+    view.ui.add(layerList, {
+      position: "bottom-right",
+      index: 0
+    });
   
-  // addWidgets(mapView);
-  // addWidgets(sceneView);
+    if (view.type=="2d") {
+      var compass = new Compass({
+        view: view
+      });
+      view.ui.add(compass, "top-left");
+    }
   
-  // mapView.when(() => {
-  //   weinLayer.queryExtent(weinQuery).then((result: any) => {
-  //     mapView.goTo(result.extent, {
-  //       animate: true,
-  //       duration: 10000,
-  //       easing: "ease-out"
-  //     });
-  //   });
-  // });
+    const scaleRangeSlider = new ScaleRangeSlider({
+      view: view,
+      layer: this.weinLayer,
+      region: "DE"
+    });
+    view.ui.add(scaleRangeSlider, "bottom-left");
+    scaleRangeSlider.watch(["minScale", "maxScale"], function(value, oldValue, name) {
+      this.weinLayer[name] = value;
+    });
   
-  // sceneView.when(() => {
-  //   weinLayer.queryExtent(weinQuery).then((result: any) => {
-  //     sceneView.goTo(result.extent, {
-  //       animate: true,
-  //       duration: 10000,
-  //       easing: "ease-out"
-  //     }).then(() => {
-  //       sceneView.watch("center", (c: Point) => {
-  //         mapView.center = c;
-  //       });
-  //     });
-  //   });
-  // });
-  
-  
-  // function addWidgets(view: View) {
-  //   var layerList = new LayerList({
-  //     view: view
-  //   });
-  //   view.ui.add(layerList, {
-  //     position: "bottom-right",
-  //     index: 0
-  //   });
-  
-  //   if (view.type=="2d") {
-  //     var compass = new Compass({
-  //       view: view
-  //     });
-  //     view.ui.add(compass, "top-left");
-  //   }
-  
-  //   const scaleRangeSlider = new ScaleRangeSlider({
-  //     view: view,
-  //     layer: weinLayer,
-  //     region: "DE"
-  //   });
-  //   view.ui.add(scaleRangeSlider, "bottom-left");
-  //   scaleRangeSlider.watch(["minScale", "maxScale"], function(value, oldValue, name) {
-  //     weinLayer[name] = value;
-  //   });
-  
-  //   var searchWidget = new Search({
-  //     view: view
-  //   });
-  //   view.ui.add(searchWidget, {
-  //     position: "top-right",
-  //     index: 0
-  //   });
-  // }
+    var searchWidget = new Search({
+      view: view
+    });
+    view.ui.add(searchWidget, {
+      position: "top-right",
+      index: 0
+    });
+  }
   
 }
 
