d8f38c9e7891b944361c53735aa4406884fcdfb6
esride-nik <n.koehn@esri.de>
2019-10-24 18:22:10 +0200

    PopupTemplate with image from feature

--- app/main.ts
+++ app/main.ts
@@ -18,10 +18,12 @@ class LearnJsapi4App {
   sceneView: SceneView;
   weinLayer: FeatureLayer;
   weinQuery: __esri.Query;
+  fotoLayer: FeatureLayer;
   
   constructor() {
     this.initializeMap();
     this.addWeinLayer();
+    this.addFotoLayer();
     this.mapView = this.viewFactory(MapView, "mapDiv");
     this.sceneView = this.viewFactory(SceneView, "sceneDiv");
     this.addCenterWatch(this.sceneView, this.sceneView);
@@ -75,6 +77,50 @@ class LearnJsapi4App {
     this.weinQuery = this.weinLayer.createQuery();
     this.weinQuery.where = "1=1";
     this.weinQuery.outFields = ["*"];
+  }
+
+  private addFotoLayer() {
+    let template = {
+      // autocasts as new PopupTemplate()
+      title: "{Titel_Hotel_Sehenswürdigkeit_L} in {Stadtbezirk}",
+      content: [
+        {
+          // It is also possible to set the fieldInfos outside of the content
+          // directly in the popupTemplate. If no fieldInfos is specifically set
+          // in the content, it defaults to whatever may be set within the popupTemplate.
+          type: "fields",
+          fieldInfos: [
+            {
+              fieldName: "Adresse_Straße_Hausnummer",
+              label: "Adresse"
+            },
+            {
+              fieldName: "Postleitzahl",
+              label: "PLZ"
+            }
+          ]
+        },
+        {
+          type: "media",
+          mediaInfos: [{
+            title: "{Titel_Hotel_Sehenswürdigkeit_L}",
+            type: "image", // Autocasts as new ImageMediaInfo()
+            caption: "{Titel_Hotel_Sehenswürdigkeit_L}",
+            // Autocasts as new ImageMediaInfoValue()
+            value: {
+              sourceURL: "{BildURL}"
+            }
+          }]
+        }
+      ]
+    };
+
+    this.fotoLayer = new FeatureLayer({
+      url: "http://services.arcgis.com/OLiydejKCZTGhvWg/arcgis/rest/services/Sehenswuerdigkeiten_POIs_3/FeatureServer/0",
+      popupTemplate: template
+    });
+    this.map.add(this.fotoLayer);
+
   }
   
   private addWidgets(view: View) {
