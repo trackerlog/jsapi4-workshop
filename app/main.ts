eb064cd76c0be794dfccec4c51b7a805f2dec064
esride-nik <n.koehn@esri.de>
2020-06-22 11:14:39 +0200

    stipdown

--- app/main.ts
+++ app/main.ts
@@ -3,56 +3,46 @@ import MapView from "esri/views/MapView";
 import FeatureLayer from "esri/layers/FeatureLayer";
 import LayerList from "esri/widgets/LayerList";
 import Compass from "esri/widgets/Compass";
-import ScaleRangeSlider from "esri/widgets/ScaleRangeSlider";
 import Search from "esri/widgets/Search";
 import SceneView from "esri/views/SceneView";
 import View from "esri/views/View";
-import { Point } from "esri/geometry";
 
-import WatchWidget from "./WatchWidget";
-import GeometryWidget from "./GeometryWidget";
-import FeatureLayerView from "esri/views/layers/FeatureLayerView";
 import GraphicsLayer from "esri/layers/GraphicsLayer";
-import Sketch from "esri/widgets/Sketch";
 import Editor from "esri/widgets/Editor";
-import Graphic from "esri/Graphic";
 
-interface ISketchCreateEvent {
-  state: string;
-  graphic: Graphic;
-}
+// interface ISketchCreateEvent {
+//   state: string;
+//   graphic: Graphic;
+// }
 
 class LearnJsapi4App {
 
-  private map : EsriMap;
+  private map: EsriMap;
   mapView: MapView;
   sceneView: SceneView;
-  weinLayer: FeatureLayer;
-  weinQuery: __esri.Query;
-  fotoLayer: FeatureLayer;
   sketchLayer: GraphicsLayer;
   editLayer: FeatureLayer;
-  
+  weinLayer: any;
+
   constructor() {
     this.initializeMap();
-    this.addWeinLayer();
-    this.addFotoLayer();
     this.addEditLayer();
-    this.sketchLayer = this.initNewGraphicsLayer("sketchLayer", "Sketch Layer");
-    this.map.add(this.sketchLayer);
-    this.mapView = this.viewFactory(MapView, "mapDiv");
-    this.sceneView = this.viewFactory(SceneView, "sceneDiv");
-    this.addCenterWatch(this.mapView, this.sceneView);
-  }
 
-  private addCenterWatch(watchView: View, setCenterView: View) {
-    let watchHandle = watchView.watch("stationary", (s: boolean) => {
-      if (s) {
-        setCenterView.watch("center", (c: Point) => {
-          this.mapView.center = c;
+    // this.sketchLayer = this.initNewGraphicsLayer("sketchLayer", "Sketch Layer");
+    // this.map.add(this.sketchLayer);
+
+    this.mapView = this.viewFactory(MapView, "mapDiv");
+
+    this.mapView.when(() => {
+      this.addWidgets(this.mapView);
+
+      let query = this.editLayer.createQuery();
+      this.editLayer.queryExtent(query).then((result: any) => {
+        this.mapView.goTo(result.extent, {
+          animate: false
         });
-        watchHandle.remove();
-      }
+      });
+
     });
   }
 
@@ -70,22 +60,10 @@ class LearnJsapi4App {
     return gl;
   }
 
-  private viewFactory<V extends View>(view: new(parameters: object) => V, containerDiv: string): V {
+  private viewFactory<V extends View>(view: new (parameters: object) => V, containerDiv: string): V {
     let initView = new view({
       map: this.map,
       container: containerDiv
-    });
-
-    initView.when(() => {
-      this.addWidgets(initView);
-
-      this.weinLayer.queryExtent(this.weinQuery).then((result: any) => {
-        initView.goTo(result.extent, {
-          animate: true,
-          duration: 10000,
-          easing: "ease-out"
-        });
-      });
     });
     return initView;
   }
@@ -97,60 +75,6 @@ class LearnJsapi4App {
     this.map.add(this.editLayer);
   }
 
-  private addWeinLayer() {
-    this.weinLayer = new FeatureLayer({
-      url: "http://services.arcgis.com/OLiydejKCZTGhvWg/arcgis/rest/services/WeinanbauGebiete/FeatureServer/0"
-    });
-    this.map.add(this.weinLayer);
-    this.weinQuery = this.weinLayer.createQuery();
-    this.weinQuery.where = "1=1";
-    this.weinQuery.outFields = ["*"];
-  }
-
-  private addFotoLayer() {
-    let template = {
-      // autocasts as new PopupTemplate()
-      title: "{Titel_Hotel_Sehenswürdigkeit_L} in {Stadtbezirk}",
-      content: [
-        {
-          // It is also possible to set the fieldInfos outside of the content
-          // directly in the popupTemplate. If no fieldInfos is specifically set
-          // in the content, it defaults to whatever may be set within the popupTemplate.
-          type: "fields",
-          fieldInfos: [
-            {
-              fieldName: "Adresse_Straße_Hausnummer",
-              label: "Adresse"
-            },
-            {
-              fieldName: "Postleitzahl",
-              label: "PLZ"
-            }
-          ]
-        },
-        {
-          type: "media",
-          mediaInfos: [{
-            title: "{Titel_Hotel_Sehenswürdigkeit_L}",
-            type: "image", // Autocasts as new ImageMediaInfo()
-            caption: "{Stadtbezirk}",
-            // Autocasts as new ImageMediaInfoValue()
-            value: {
-              sourceURL: "{BildURL}"
-            }
-          }]
-        }
-      ]
-    };
-
-    this.fotoLayer = new FeatureLayer({
-      url: "http://services.arcgis.com/OLiydejKCZTGhvWg/arcgis/rest/services/Sehenswuerdigkeiten_POIs_3/FeatureServer/0",
-      popupTemplate: template
-    });
-    this.map.add(this.fotoLayer);
-
-  }
-  
   private addWidgets(view: View) {
     let layerList = new LayerList({
       view: view
@@ -159,27 +83,14 @@ class LearnJsapi4App {
       position: "bottom-right",
       index: 0
     });
-  
-    if (view.type=="2d") {
+
+    if (view.type == "2d") {
       let compass = new Compass({
         view: view
       });
       view.ui.add(compass, "top-left");
     }
-  
-    let scaleRangeSlider = new ScaleRangeSlider({
-      view: view,
-      layer: this.weinLayer,
-      region: "DE"
-    });
-    view.ui.add(scaleRangeSlider, {
-      position: "top-left",
-      index: 0
-    });
-    scaleRangeSlider.watch(["minScale", "maxScale"], function(value, oldValue, name) {
-      this.weinLayer[name] = value;
-    });
-  
+
     let searchWidget = new Search({
       view: view
     });
@@ -196,49 +107,32 @@ class LearnJsapi4App {
       index: 0
     });
 
-    var sketch = new Sketch({
-      layer: this.sketchLayer,
-      view: view
-    });
-    // Listen to sketch widget's create event.
-    sketch.on("create", (event: ISketchCreateEvent) => {
-      if (event.state === "complete") {
-       if(event.graphic.geometry.type === "polygon") {
-        if (this.editLayer) {
-          this.editLayer.applyEdits({
-            addFeatures: [event.graphic]
-          });
-          this.sketchLayer.remove(event.graphic);
-        }
-        else {
-          console.warn("Editierlayer ungültig.");
-        }
-       }
-      }
-    });
-    view.ui.add(sketch, {
-      position: "top-right",
-      index: 0
-    });
+    //   var sketch = new Sketch({
+    //     layer: this.sketchLayer,
+    //     view: view
+    //   });
+    //   // Listen to sketch widget's create event.
+    //   sketch.on("create", (event: ISketchCreateEvent) => {
+    //     if (event.state === "complete") {
+    //      if(event.graphic.geometry.type === "polygon") {
+    //       if (this.editLayer) {
+    //         this.editLayer.applyEdits({
+    //           addFeatures: [event.graphic]
+    //         });
+    //         this.sketchLayer.remove(event.graphic);
+    //       }
+    //       else {
+    //         console.warn("Editierlayer ungültig.");
+    //       }
+    //      }
+    //     }
+    //   });
+    //   view.ui.add(sketch, {
+    //     position: "top-right",
+    //     index: 0
+    //   });
 
-    let geometryWidget = new GeometryWidget(view);
-    geometryWidget.setSelectionTarget(this.weinLayer);
-    geometryWidget.setEditLayer(this.editLayer);
-    view.whenLayerView(this.weinLayer).then((layerView: FeatureLayerView) => {
-      geometryWidget.setLayerView(layerView);
-    });
-    view.ui.add(geometryWidget, {
-      position: "top-right",
-      index: 3
-    });
-
-    let watchWidget = new WatchWidget(view)
-    view.ui.add(watchWidget, {
-      position: "bottom-left",
-      index: 0
-    });
   }
-  
 }
 
 let app = new LearnJsapi4App();
