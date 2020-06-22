5e31fc24484c0cc9c333141ac60bf4bf5867f0dc
esride-nik <n.koehn@esri.de>
2020-06-22 12:12:15 +0200

    selecting complete symbol by clicking graphic

--- app/main.ts
+++ app/main.ts
@@ -12,6 +12,8 @@ import Handle from "esri/core/Handles";
 import Renderer from "esri/renderers/Renderer";
 import Legend from "esri/widgets/Legend";
 import WatchWidget from "./WatchWidget";
+import Graphic from "esri/Graphic";
+import FeatureSet from "esri/tasks/support/FeatureSet";
 
 // interface ISketchCreateEvent {
 //   state: string;
@@ -28,7 +30,6 @@ class LearnJsapi4App {
   editLayer: FeatureLayer;
   
   // Complex Symbol sample vars
-  private citiesFlv: FeatureLayerView;
   private innerHighlightHandle: Handle;
   private outerHighlightHandle: Handle;
   private iconPaths = {
@@ -36,8 +37,10 @@ class LearnJsapi4App {
     "smiley": "M24.0,2.199C11.9595,2.199,2.199,11.9595,2.199,24.0c0.0,12.0405,9.7605,21.801,21.801,21.801c12.0405,0.0,21.801-9.7605,21.801-21.801C45.801,11.9595,36.0405,2.199,24.0,2.199zM31.0935,11.0625c1.401,0.0,2.532,2.2245,2.532,4.968S32.4915,21.0,31.0935,21.0c-1.398,0.0-2.532-2.2245-2.532-4.968S29.697,11.0625,31.0935,11.0625zM16.656,11.0625c1.398,0.0,2.532,2.2245,2.532,4.968S18.0555,21.0,16.656,21.0s-2.532-2.2245-2.532-4.968S15.258,11.0625,16.656,11.0625zM24.0315,39.0c-4.3095,0.0-8.3445-2.6355-11.8185-7.2165c3.5955,2.346,7.5315,3.654,11.661,3.654c4.3845,0.0,8.5515-1.47,12.3225-4.101C32.649,36.198,28.485,39.0,24.0315,39.0z"
   }
   private initColor = "#ce641d";
-  weatherSvgLayer: any;
-  hwyLayer: any;
+  private weatherSvgLayer: any;
+  private weatherSimpleLayer: FeatureLayer;
+  private weatherSvgFlv: FeatureLayerView;
+  private weatherSimpleFlv: FeatureLayerView;
   
 
   constructor() {
@@ -97,12 +100,12 @@ class LearnJsapi4App {
       }
     } as unknown as Renderer;
 
-    this.weatherSvgLayer = this.createWeatherLayer("citiesSvg", svgRenderer);
-    let weatherSimpleLayer = this.createWeatherLayer("citiesSimple", circleRenderer);
+    this.weatherSvgLayer = this.createWeatherLayer("weatherSvg", svgRenderer);
+    this.weatherSimpleLayer = this.createWeatherLayer("weatherSimple", circleRenderer);
 
     const mapProperties = {
       basemap: "gray-vector",
-      layers: [this.weatherSvgLayer, weatherSimpleLayer]
+      layers: [this.weatherSvgLayer, this.weatherSimpleLayer]
     };
 
     this.map = new EsriMap(mapProperties);
@@ -160,18 +163,27 @@ class LearnJsapi4App {
     view.on("click", (event: any) => {
       view.hitTest(event).then((response: any) => {
         if (response.results.length) {
-          let svgResults = response.results.filter((result: any) => result.graphic.layer.id === "citiesSimple");
-          console.log("Hit test", response, svgResults);
+          let svgResults = response.results.filter((result: any) => result.graphic.layer.id === "weatherSvg");
           svgResults.forEach((svgResult: any) => {
-            this.innerHighlightHandle = this.citiesFlv.highlight(svgResult.graphic) as unknown as Handle;
-            console.log("highlight", svgResult.graphic);
+            let svgFeature: Graphic = svgResult.graphic;
+            let objectIdField = (svgFeature.layer as FeatureLayer).objectIdField;
+            this.innerHighlightHandle = this.weatherSvgFlv.highlight(svgFeature) as unknown as Handle;
+            
+            let weatherSimpleQuery = this.weatherSimpleLayer.createQuery();
+            weatherSimpleQuery.where = objectIdField + "=" + svgFeature.attributes[objectIdField];
+            this.weatherSimpleLayer.queryFeatures(weatherSimpleQuery).then((response: FeatureSet) => {
+              this.outerHighlightHandle = this.weatherSimpleFlv.highlight(response.features[0]) as unknown as Handle;
+            });
           });
         }
       });
     });
 
     view.whenLayerView(this.weatherSvgLayer).then((flv: FeatureLayerView) => {
-      this.citiesFlv = flv;
+      this.weatherSvgFlv = flv;
+    });
+    view.whenLayerView(this.weatherSimpleLayer).then((flv: FeatureLayerView) => {
+      this.weatherSimpleFlv = flv;
     });
 
     return view;
