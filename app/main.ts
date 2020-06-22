90ab9a758b5173f0cd706bdd21c33cb3aaa33f54
esride-nik <n.koehn@esri.de>
2020-06-22 13:53:05 +0200

    array of handles

--- app/main.ts
+++ app/main.ts
@@ -32,8 +32,8 @@ class LearnJsapi4App {
   editLayer: FeatureLayer;
   
   // Complex Symbol sample vars
-  private innerHighlightHandle: Handle;
-  private outerHighlightHandle: Handle;
+  private innerHighlightHandles: Handle[] = [];
+  private outerHighlightHandles: Handle[] = [];
   private iconPaths = {
     "arrow": "M14.5,29 23.5,0 14.5,9 5.5,0z",
     "smiley": "M24.0,2.199C11.9595,2.199,2.199,11.9595,2.199,24.0c0.0,12.0405,9.7605,21.801,21.801,21.801c12.0405,0.0,21.801-9.7605,21.801-21.801C45.801,11.9595,36.0405,2.199,24.0,2.199zM31.0935,11.0625c1.401,0.0,2.532,2.2245,2.532,4.968S32.4915,21.0,31.0935,21.0c-1.398,0.0-2.532-2.2245-2.532-4.968S29.697,11.0625,31.0935,11.0625zM16.656,11.0625c1.398,0.0,2.532,2.2245,2.532,4.968S18.0555,21.0,16.656,21.0s-2.532-2.2245-2.532-4.968S15.258,11.0625,16.656,11.0625zM24.0315,39.0c-4.3095,0.0-8.3445-2.6355-11.8185-7.2165c3.5955,2.346,7.5315,3.654,11.661,3.654c4.3845,0.0,8.5515-1.47,12.3225-4.101C32.649,36.198,28.485,39.0,24.0315,39.0z"
@@ -193,16 +193,18 @@ class LearnJsapi4App {
             this.weatherSimpleLayer.queryFeatures(weatherSimpleQuery).then((response: FeatureSet) => {
               console.log(this.ctrlKeyDown);
               
-              if (this.ctrlKeyDown === false) {
-                if (this.innerHighlightHandle) {
-                  this.innerHighlightHandle.remove();
+              if (!this.ctrlKeyDown) {
+                if (this.innerHighlightHandles.length>0) {
+                  this.innerHighlightHandles.forEach((iHH: Handle) => iHH.remove());
+                  this.innerHighlightHandles = [];
                 }
-                if (this.outerHighlightHandle) {
-                  this.outerHighlightHandle.remove();
+                if (this.outerHighlightHandles) {
+                  this.outerHighlightHandles.forEach((oHH: Handle) => oHH.remove());
+                  this.outerHighlightHandles = [];
                 }
               }
-              this.innerHighlightHandle = this.weatherSvgFlv.highlight(svgFeature) as unknown as Handle;
-              this.outerHighlightHandle = this.weatherSimpleFlv.highlight(response.features[0]) as unknown as Handle;
+              this.innerHighlightHandles.push(this.weatherSvgFlv.highlight(svgFeature) as unknown as Handle);
+              this.outerHighlightHandles.push(this.weatherSimpleFlv.highlight(response.features[0]) as unknown as Handle);
             });
           });
         }
